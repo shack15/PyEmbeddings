3427353ff43379519ffb8c2c92c5b4d875c4233d
Anant <anantsnh@umich.edu>
2023-12-07 15:27:43 -0800

    Initial Version 0.1.0 release

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='embedding',  
+    name='pyembeddings',  
     version='0.1.0',
     author='Anant Sinha',
     author_email='anant@shack15.com',
     description='Simple and intuitive embeddings generation, storage, and retrieval.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/shack15/embedding',
+    url='https://github.com/shack15/PyEmbeddings',
     packages=find_packages(),
     install_requires=[
         'requests',
@@ -17,7 +17,6 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',  # Change as appropriate
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',  # Adjust the license accordingly
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
