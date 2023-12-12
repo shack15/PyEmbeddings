57005b74895da583502b5e3620fa9c22270ae78a
Anant <anantsnh@umich.edu>
2023-12-11 20:00:21 -0800

    Add test

--- pyembeddings/__init__.py
+++ pyembeddings/__init__.py
@@ -4,6 +4,43 @@
 api_key = None
 embedding_model = "MiniLM"
 
+# data on the models that we host
+
+models_info = {
+    "bge-small": {
+        "full_name": "BAAI/bge-small-en-v1.5",
+        "type": "text",
+        "description": "General purpose embedding model",
+        "dimensions": 384,
+        "token_limit": 512,
+        "pricing": "0.0001 per embedding",
+    },
+    "bge-base": {
+        "full_name": "BAAI/bge-base-en-v1.5",
+        "type": "text",
+        "description": "General purpose embedding model",
+        "dimensions": 768,
+        "token_limit": 512,
+        "pricing": "0.0001 per embedding",
+    },
+    "MiniLM": {
+        "full_name": "sentence-transformers/all-MiniLM-L6-v2",
+        "type": "text",
+        "description": "General purpose embedding model",
+        "dimensions": 384,
+        "token_limit": 512,
+        "pricing": "0.00001 per embedding",
+    },
+    "jina-small": {
+        "full_name": "jinaai/jina-embeddings-v2-small-en",
+        "type": "text",
+        "description": "General purpose embedding model",
+        "dimensions": 512,
+        "token_limit": 8192,
+        "pricing": "0.00001 per embedding",
+    }
+}
+
 def init(key):
     global api_key
     api_key = key
