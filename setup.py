86194da3b5dab9963be7f76c2b7e4b875e7ea9ce
Anant <anantsnh@umich.edu>
2024-01-22 14:12:44 -0800

    Update version for package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyembeddings',  
-    version='0.1.0',
+    version='0.1.1',
     author='Anant Sinha',
     author_email='anant@shack15.com',
     description='Simple and intuitive embeddings generation, storage, and retrieval.',
